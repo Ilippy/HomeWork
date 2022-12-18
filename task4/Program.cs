befc09b9a0e4f50f76feeff25122ea4cd94c04e1
Lirian <Ilippy@users.noreply.github.com>
2022-12-18 23:48:10 +0300

    bugfix
    You forgot $ at Console.WriteLine($"{a}, {b}, {c} -> {max}");

--- task4/Program.cs
+++ task4/Program.cs
@@ -10,7 +10,6 @@ int max = a;
 if(a > max) max = a;
 if(b > max) max = b;
 if(c > max) max = c;
-Console.Write ("max = ");
-Console.WriteLine("{a}, {b}, {c} -> {max}");
+Console.WriteLine($"{a}, {b}, {c} -> {max}");
         
     
